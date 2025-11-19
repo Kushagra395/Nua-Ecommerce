f91820be588986577fcd447b9b04f8e65b1edb57
kushagra <kushagramalviya3@gmail.com>
2025-11-19 16:57:27 +0530

    installment and setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
